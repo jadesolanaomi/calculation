9cc6aa11d2776077af0a10f3fff32ff12693c696
jadesolanaomi <naomiadeyinka3@gmai.com>
2024-05-02 09:25:08 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//document.querySelector("p").innerHTML = "welcome to Dom";
+
+//document.querySelectorAll("p")[0].innerHTML = "welcome to Dom 1";
+//document.querySelectorAll("p")[1].innerHTML = "welcome to Dom 2";
+
+//document.querySelector(".result").innerHTML = "2+ 2 =4";
+
+
+//function sum(){
+  //  let num1= document.querySelector("#num1").value;//20
+    //let num2= document.querySelector("#num2").value;//8
+
+    //let total =+num1 + +num2;
+
+    //console.log(total);
+    //document.querySelector("#result").innerHTML = total;
+}
+
+
+function subtract(){
+    let num1= document.querySelector("#num1").value;//20
+    let num2=document.querySelector("#num2").value;//8
+
+    let total =+num1 - +num2;
+    
+    console.log(total);
+    
+    document.querySelector("#result").innerHTML =total;
+}
+
+
+
+
